6c0b975ff4ac58998f9f1e437681288bffe44c26
haje01 <haje01@gmail.com>
2014-11-17 08:05:01 +0000

    mkdir conv, hdf, tmp

--- wzdat/jobs.py
+++ wzdat/jobs.py
@@ -73,6 +73,7 @@ def register_cron():
     from wzdat.ipynb_runner import find_cron_notebooks, register_cron_notebooks
     nb_dir = get_notebook_dir()
     paths, scheds, _, _ = find_cron_notebooks(nb_dir)
+    logging.debug("register_cron " + str(paths))
     register_cron_notebooks(paths, scheds)
 
 
