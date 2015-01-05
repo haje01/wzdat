fff026e5d4a26038cb2eea9b79907a86b3336cf5
haje01 <haje01@gmail.com>
2015-01-05 17:45:25 +0900

    add test_system; let drone run system test

---
+++ system/tests/test_system.py
@@ -0,0 +1,31 @@
+import os
+import re
+from subprocess import check_output
+
+import pytest
+
+host = os.environ['WZDAT_HOST']
+env = os.environ.copy()
+env['RSYNC_PASSWORD'] = 'test'
+
+
+@pytest.yield_fixture(scope='module')
+def rport():
+    ret = check_output(['docker', 'ps'])
+    port = re.search(r'.*:(\d+)->873.*', ret).groups()[0]
+    yield port
+
+
+def test_system_dashboard():
+    import urllib2
+    url = 'http://{}:8085'.format(host)
+    f = urllib2.urlopen(url)
+    r = f.read()
+    assert 'WzDat MYPRJ Dashboard' in r
+
+
+def test_system_rsync(rport):
+    cmd = ['rsync', '-azv', 'LICENSE', '--port={}'.format(rport),
+           'rsync-user@{}::rsync-data/test'.format(host)]
+    ret = check_output(cmd, env=env)
+    assert 'sent ' in ret
