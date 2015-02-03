39eede765c59ce009505e6a31b812cc0615dd7ca
haje01 <haje01@gmail.com>
2015-02-03 18:48:30 +0900

    fix start caching fail

--- system/files/start.sh
+++ system/files/start.sh
@@ -5,11 +5,13 @@ echo "alias ll='ls -alF'" > ~/.bash_profile
 echo "export EDITOR=vi" >> ~/.bash_profile
 echo "export WZDAT_CFG=$WZDAT_CFG" >> ~/.bash_profile
 echo "export WZDAT_DIR=$WZDAT_DIR" >> ~/.bash_profile
+source ~/.bash_profile
 mkdir -p /logdata/_var_
 cd /wzdat
 pip install -e .
 # Re-install of requirements to apply changes after base image build.
 pip install -r requirements.txt
+cd /solution
 python -m wzdat.rundb create  # create db table if not exists
 python -m wzdat.jobs cache-all
 /usr/bin/supervisord
