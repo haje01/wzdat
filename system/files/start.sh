879011714f162e79c6c701216a71421d84f7241a
haje01 <haje01@gmail.com>
2015-02-04 15:41:37 +0900

    fix typo

--- system/files/start.sh
+++ system/files/start.sh
@@ -5,7 +5,7 @@ echo "alias ll='ls -alF'" > ~/.bash_profile
 echo "export EDITOR=vi" >> ~/.bash_profile
 echo "export WZDAT_CFG=$WZDAT_CFG" >> ~/.bash_profile
 echo "export WZDAT_DIR=$WZDAT_DIR" >> ~/.bash_profile
-echo "export WZDAT_DIR=$WZDAT_HOST" >> ~/.bash_profile
+echo "export WZDAT_HOST=$WZDAT_HOST" >> ~/.bash_profile
 source ~/.bash_profile
 mkdir -p /logdata/_var_
 cd /wzdat
