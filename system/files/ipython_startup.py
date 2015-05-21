49b97802db0e1209d95164ab8fd291b283fe89ce
JeongJu Kim <haje01@gmail.com>
2015-05-21 08:55:58 +0000

    disable hdf table

--- system/files/ipython_startup.py
+++ system/files/ipython_startup.py
@@ -5,7 +5,7 @@ mlp.rcParams['font.family'] = u'NanumGothic'
 mlp.rcParams['font.size'] = 10
 
 import pandas as pd
-pd.set_option('io.hdf.default_format', 'table')  # default hdf format 'table'
+# pd.set_option('io.hdf.default_format', 'table')  # default hdf format 'table'
 
 from pandas import Series, DataFrame
 import numpy as np
